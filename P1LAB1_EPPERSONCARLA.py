cf3645a989c8cffe37884de56b63709c759deec9
EppersonCarla <160422122+EppersonCarla@users.noreply.github.com>
2024-09-23 21:47:26 -0400

    Add files via upload

---
+++ P1LAB1_EPPERSONCARLA.py
@@ -0,0 +1,16 @@
+"""
+#Name: Carla Epperson
+
+#Date: September 23,2024
+
+#Assignment name: P1LAB1
+
+#Welcome Message
+
+"""
+
+first_name = input ('Please enter your first name:')
+                    
+last_name = input ('Please enter your last name:')
+
+print(f'Hello, {first_name} {last_name}! Welcome to CTI-110.')
